ff22315c89b908e81ec11755b1dac5999ca4dbdc
Seth Scott <sealscott8904@gmail.com>
2025-05-06 01:25:45 -0400

    Added config and SSL

---
+++ src/PHP-Backend/config/config.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'DB_HOST' => 'cs330-2025-01-group03-db.mysql.database.azure.com', // Replace with your Azure hostname
+    'DB_PORT' => '3306', // Default MySQL port (or replace with the port provided by Azure)
+    'DB_NAME' => 'weathermap', // Replace with your database name
+    'DB_USER' => 'mysqladmin', // Replace with your username
+    'DB_PASS' => 'EatShitPitt!2025.01', // Replace with your password
+    'DB_SSL'  => 'require', // SSL mode (Azure requires SSL)
+];
