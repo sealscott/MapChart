43d0d3ee44e663b3175ea2221456bcfa3c43f82b
Seth Scott <143134648+sealscott@users.noreply.github.com>
2025-05-05 22:41:02 -0400

    Update map-page.php

--- src/Pages/map-page.php
+++ src/Pages/map-page.php
@@ -13,7 +13,7 @@ if (isset($_COOKIE['darkMode']) && $_COOKIE['darkMode'] === 'enabled') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <body class="<?php echo $darkMode; ?>">
-    <title>WeatherMap Social</title>
+    <title>MapChart</title>
 
     <link id="mainStylesheet" rel="stylesheet" href="/src/CSS/main.css">
     <link id="mapStylesheet"rel="stylesheet" href="/src/CSS/map.css">
@@ -73,7 +73,7 @@ if (isset($_COOKIE['darkMode']) && $_COOKIE['darkMode'] === 'enabled') {
     <div id="map-container">
         <!-- Header inside map with title and search -->
         <div id="map-header">
-            <div id="app-title">WeatherMap Social</div>
+            <div id="app-title">MapChart</div>
             <input type="text" id="searchBox" placeholder="Find posts in any area">
             <button id="searchButton">Search</button>
         </div>
